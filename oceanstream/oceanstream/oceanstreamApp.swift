71532a675565dce4cd07f941a478f83690d127f0
miguelvneto <miguelvieiraneto1@gmail.com>
2025-09-17 11:37:01 -0700

    bkp

---
+++ oceanstream/oceanstream/oceanstreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  oceanstreamApp.swift
+//  oceanstream
+//
+//  Created by Environlink on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct oceanstreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ oceanstream/oceanstreamTests/oceanstreamTests.swift
@@ -0,0 +1,16 @@
+//
+//  oceanstreamTests.swift
+//  oceanstreamTests
+//
+//  Created by Environlink on 8/27/25.
+//
+
+import Testing
+
+struct oceanstreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
